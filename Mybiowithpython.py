341114826e1f1ab0a6330805e45a75183e6669a5
Kevin Pandu <162404229+Infoonlygit@users.noreply.github.com>
2024-04-20 16:40:23 +0700

    Update and rename fbcreate.py to Mybiowithpython.py

---
+++ Mybiowithpython.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+print(" _  __          _          ____                 
+| |/ /_____   _(_)_ __    / ___| __ _ _ __  ____
+| ' // _ \ \ / / | '_ \  | |  _ / _` | '_ \|_  /
+| . \  __/\ V /| | | | | | |_| | (_| | | | |/ / 
+|_|\_\___| \_/ |_|_| |_|  \____|\__,_|_| |_/___|")
+
+
+print("Nama: Kevin")
+print("Usia: 14 tahun")
+print("Kelas: 8")
+print("Language: Python")
+print("Hobby: Programming")
